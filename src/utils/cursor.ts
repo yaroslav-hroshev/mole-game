0a5e4beca55093883014dbc4aeadcfdf748e1768
yaroslav-hroshev <yaroslav@mgroupweb.com>
2024-09-02 15:20:14 +0300

    Custom cursor is completed without animation

--- src/utils/cursor.ts
+++ src/utils/cursor.ts
@@ -1,3 +1,6 @@
+import { LS_TRANSFORM } from '../constants/constants';
+import { getTransformValues, TransformValues, setTransformValue } from './cursorHelper';
+
 // site wide cursor
 const siteWideCursor: HTMLDivElement | null = document.querySelector('.site-wide.custom-cursor');
 
@@ -5,32 +8,57 @@ document.addEventListener('mouseenter', (): void => {
   if (siteWideCursor) {
     siteWideCursor.style.display = 'block';
   }
-})
+});
 
 document.addEventListener('mouseleave', (): void => {
   if (siteWideCursor) {
     siteWideCursor.style.display = 'none';
   }
-})
+});
 
 document.addEventListener('mousedown', (): void => {
-  if (siteWideCursor) {
+  if (siteWideCursor && siteWideCursor.querySelector('.background-image')) {
+    setTransformValue(siteWideCursor, LS_TRANSFORM, 45);
+  } else if (siteWideCursor) {
     siteWideCursor.classList.add('active');
   }
-})
+});
+
 document.addEventListener('mouseup', (): void => {
-  if (siteWideCursor) {
+  if (siteWideCursor && siteWideCursor.querySelector('.background-image')) {
+    setTransformValue(siteWideCursor, LS_TRANSFORM, 0);
+  } else if (siteWideCursor) {
     siteWideCursor.classList.remove('active');
   }
-})
+});
 
 document.addEventListener('mousemove', TrackCursorMouse);
 
 function TrackCursorMouse(event: MouseEvent): void {
   if (siteWideCursor) {
-    const { clientX, clientY }: {clientX: number; clientY: number} = event;
+    const { clientX, clientY }: { clientX: number; clientY: number } = event;
     const x: number = siteWideCursor.clientWidth;
     const y: number = siteWideCursor.clientHeight;
-    siteWideCursor.style.transform = `translate(${clientX - x/2}px, ${clientY - y/2}px)`;
+
+    const valuesFromLS: TransformValues | null = window.localStorage.getItem(LS_TRANSFORM)
+      ? (JSON.parse(window.localStorage.getItem(LS_TRANSFORM) as string) as TransformValues)
+      : null;
+    console.log(valuesFromLS);
+    let transform;
+    if (valuesFromLS) {
+      transform = `translate(${clientX - x / 2}px, ${clientY - y / 2}px) rotate(${valuesFromLS.rotation}deg)`;
+    } else {
+      transform = `translate(${clientX - x / 2}px, ${clientY - y / 2}px) rotate(0deg)`;
+    }
+    siteWideCursor.style.transform = transform;
+    // const transformValues: TransformValues | null = getTransformValues(siteWideCursor);
+    // window.localStorage.setItem(`${LS_TRANSFORM}`, JSON.stringify(transformValues));
+    if (
+      event.target !== null &&
+      event.target instanceof Element &&
+      event.target.closest('container-element')
+    ) {
+      siteWideCursor.querySelector('.pointer')?.classList.add('background-image');
+    }
   }
 }
---
+++ src/utils/cursorHelper.ts
@@ -0,0 +1,32 @@
+export type TransformValues = {
+  translateX: string;
+  translateY: string;
+  rotation: number;
+};
+
+export const getTransformValues = (element: HTMLElement): TransformValues | null => {
+  const elementStyle: CSSStyleDeclaration = window.getComputedStyle(element);
+  const transformValue: string = elementStyle.getPropertyValue('transform');
+  if (transformValue && transformValue.startsWith('matrix')) {
+    const matrixValues: RegExpMatchArray | null = transformValue.match(/matrix\(([^)]+)\)/);
+    if (matrixValues) {
+      const [_, a, b, c, x, y, f]: string[] = matrixValues[1].split(', ');
+      const transformValues: TransformValues = {
+        translateX: x,
+        translateY: y,
+        rotation: (Math.round(Math.atan2(parseFloat(b), parseFloat(a)) * (180/Math.PI))),
+      };
+      return transformValues;
+    }
+  }
+  return null;
+};
+
+export const setTransformValue = (element: HTMLElement, lsVariable: string, rotateDeg: number): void => {
+  const transformValues: TransformValues | null = getTransformValues(element);
+  if (transformValues !== null && transformValues) {
+    const { translateX, translateY } = transformValues;
+    element.style.transform = `translate(${translateX}px, ${translateY}px) rotate(-${rotateDeg}deg)`;
+    window.localStorage.setItem(`${lsVariable}`, JSON.stringify(transformValues));
+  }
+};
